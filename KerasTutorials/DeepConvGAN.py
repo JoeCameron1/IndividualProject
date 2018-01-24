93be677e22c07e50ae87b0b880d35671a3a9eb06
Joseph Cameron <jcameron97@btinternet.com>
2018-01-24 22:25:35 +0000

    Adding my first GAN tutorial/implementation

---
+++ KerasTutorials/DeepConvGAN.py
@@ -0,0 +1,188 @@
+# Deep Convolutional GAN Example Tutorial
+# Inspired by https://github.com/soumith/ganhacks,
+# https://github.com/eriklindernoren/Keras-GAN and the paper https://arxiv.org/pdf/1511.06434.pdf
+
+# NOTE: Must include an empty directory called 'images' in the same directory as this script to save images
+
+# Import print function and division from python3 into python2
+from __future__ import print_function, division
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.models import Sequential, Model
+from keras.optimizers import Adam
+
+
+class DCGAN():
+    
+    def __init__(self):
+        self.img_rows = 28 
+        self.img_cols = 28
+        self.channels = 1
+
+        optimizer = Adam(0.0002, 0.5)
+
+        # Build the discriminator
+        self.discriminator = self.build_discriminator()
+        # Compile the discriminator
+        self.discriminator.compile(loss='binary_crossentropy', 
+            optimizer=optimizer,
+            metrics=['accuracy'])
+
+        # Build the generator
+        self.generator = self.build_generator()
+        # Compile the generator
+        self.generator.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+        # The generator takes noise as input and generated imgs
+        z = Input(shape=(100,))
+        img = self.generator(z)
+
+        # For the combined model we will only train the generator
+        self.discriminator.trainable = False
+
+        # The valid takes generated images as input and determines validity
+        valid = self.discriminator(img)
+
+        # The combined model  (stacked generator and discriminator) takes
+        # noise as input => generates images => determines validity 
+        self.combined = Model(z, valid)
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+
+    def build_generator(self):
+
+        noise_shape = (100,)
+        
+        model = Sequential()
+        model.add(Dense(128 * 7 * 7, activation="relu", input_shape=noise_shape))
+        model.add(Reshape((7, 7, 128)))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(UpSampling2D())
+        model.add(Conv2D(128, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8)) 
+        model.add(UpSampling2D())
+        model.add(Conv2D(64, kernel_size=3, padding="same"))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(1, kernel_size=3, padding="same"))
+        # Hint from https://github.com/soumith/ganhacks
+        # Tanh as the last layer of the generator output
+        model.add(Activation("tanh"))
+
+        model.summary()
+
+        noise = Input(shape=noise_shape)
+        img = model(noise)
+
+        return Model(noise, img)
+
+    def build_discriminator(self):
+
+        img_shape = (self.img_rows, self.img_cols, self.channels)
+        
+        model = Sequential()
+        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=img_shape, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
+        model.add(ZeroPadding2D(padding=((0,1),(0,1))))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
+        model.add(LeakyReLU(alpha=0.2))
+        model.add(Dropout(0.25))
+        model.add(Flatten())
+        model.add(Dense(1, activation='sigmoid'))
+
+        model.summary()
+
+        img = Input(shape=img_shape)
+        validity = model(img)
+
+        return Model(img, validity)
+
+    def train(self, epochs, batch_size=128, save_interval=50):
+
+        # Load the MNIST dataset
+        (X_train, _), (_, _) = mnist.load_data()
+
+        # Rescale -1 to 1
+        # Hint from https://github.com/soumith/ganhacks
+        X_train = (X_train.astype(np.float32) - 127.5) / 127.5
+        X_train = np.expand_dims(X_train, axis=3)
+
+        half_batch = int(batch_size / 2)
+
+        for epoch in range(epochs):
+
+            # ---------------------
+            #  TRAIN DISCRIMINATOR
+
+            # Take half of a batch of images randomly
+            idx = np.random.randint(0, X_train.shape[0], half_batch)
+            imgs = X_train[idx]
+
+            # Sample noise and generate a half batch of new images
+            noise = np.random.normal(0, 1, (half_batch, 100))
+            gen_imgs = self.generator.predict(noise)
+
+            # Train the discriminator
+            # Real images are classified as 1
+            # Generated images are classified as 0
+            d_loss_real = self.discriminator.train_on_batch(imgs, np.ones((half_batch, 1)))
+            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, np.zeros((half_batch, 1)))
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  TRAIN GENERATOR
+
+            noise = np.random.normal(0, 1, (batch_size, 100))
+
+            # Train the generator
+            # Aims to fool the discriminator into thinking the images are real
+            g_loss = self.combined.train_on_batch(noise, np.ones((batch_size, 1)))
+
+            # ---------------------
+
+            # Plot the progress
+            print ("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
+
+            # If at save interval => save generated image samples
+            if epoch % save_interval == 0:
+                self.saveImages(epoch)
+
+    def saveImages(self, epoch):
+        r, c = 5, 5
+        noise = np.random.normal(0, 1, (r * c, 100))
+        gen_imgs = self.generator.predict(noise)
+
+        # Rescale images 0 - 1
+        gen_imgs = 0.5 * gen_imgs + 0.5
+
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        fig.savefig("images/mnist_%d.png" % epoch)
+        plt.close()
+
+
+if __name__ == '__main__':
+    dcgan = DCGAN()
+    dcgan.train(epochs=4000, batch_size=32, save_interval=50)
