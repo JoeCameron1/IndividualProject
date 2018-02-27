ad3ce53e9c63fbfd2f9ded7e3d7d3346fd5f2b09
Joseph Cameron <jcameron97@btinternet.com>
2018-02-27 22:12:12 +0000

    Confusion Matrix

--- KnotClassifier/knotClassifier.py
+++ KnotClassifier/knotClassifier.py
@@ -204,3 +204,69 @@ plotHistoryLoss(fit)
 plotHistoryAccuracy(fit)
 historyFigure.savefig('trainingHistory.jpg')
 plt.close()
+
+# -----------------------------------------------------------
+
+# PLOT CONFUSION MATRIX
+
+matrix_predictions = model.predict_generator(validation_generator)
+matrix_predictions = np.argmax(matrix_predictions, axis=-1)
+print validation_generator.classes
+print matrix_predictions
+
+confusionMatrix = confusion_matrix(validation_generator.classes, matrix_predictions)
+class_names = ['Alpine Butterfly Knot', 'Bowline Knot', 'Clove Hitch', 'Figure-8 Knot', 'Figure-8 Loop',
+               'Fisherman Knot', 'Flemish Bend', 'Overhand Knot', 'Reef Knot', 'Slip Knot']
+
+# plot_confusion_matrix function obtained from sklearn documentation
+# Confusion Matrix Examples
+# http://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+np.set_printoptions(precision=2)
+
+# Plot Confusion Matrix
+final_confusion_matrix = plt.figure(figsize=(20, 10))
+plot_confusion_matrix(confusionMatrix, classes=class_names,
+                      title='Confusion Matrix')
+final_confusion_matrix.savefig('confusionMatrix.jpg')
+
+# Plot Normalised Confusion Matrix
+final_normalised_confusion_matrix = plt.figure(figsize=(20, 10))
+plot_confusion_matrix(confusionMatrix, classes=class_names, normalize=True,
+                      title='Normalised Confusion Matrix')
+final_normalised_confusion_matrix.savefig('normalisedConfusionMatrix.jpg')
+
+plt.close()
