c1171e5bee07a1f3826b3bffdc38a3b727eed66b
Joseph Cameron <jcameron97@btinternet.com>
2018-03-19 15:00:27 +0000

    Adding large CNN model architecture

--- KnotClassifier/knotClassifier.py
+++ KnotClassifier/knotClassifier.py
@@ -125,21 +125,27 @@ elif args.large:
     
     model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     
-    #model.add(Dropout(0.5))
-    
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
-    #model.add(BatchNormalization())
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    
     model.add(Dropout(0.5))
     
+#    model.add(Conv2D(128, (3, 3)))
+#    model.add(Activation('relu'))
+#    model.add(Conv2D(128, (3, 3)))
+#    model.add(Activation('relu'))
+#    model.add(MaxPooling2D(pool_size=(2, 2)))
+    
     model.add(Flatten())
+    model.add(Dense(128))
+    model.add(Activation('relu'))
     model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
@@ -225,6 +231,7 @@ if args.augmentation:
     )
 
 # NO DATA AUGMENTATION
+
 else:
     
     print 'You are not using data augmentation'
